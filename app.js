576915818af39588089a3f7617fc4548f4ee1bee
MuhammadAnnasRazajs <muhammadannasraza786@gmail.com>
2024-09-29 16:09:35 +0500

    Assignment-Completed

---
+++ app.js
@@ -0,0 +1,47 @@
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+
+const display = document.querySelector('#display');
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const resetButton = document.querySelector('#reset');
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 1000);
+        running = true;
+    }
+}
+
+function stopTimer() {
+    if (running) {
+        clearInterval(tInterval);
+        running = false;
+    }
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    difference = 0;
+    display.innerHTML = "00:00:00";
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    display.innerHTML = 
+        (hours < 10 ? "0" : "") + hours + ":" + 
+        (minutes < 10 ? "0" : "") + minutes + ":" + 
+        (seconds < 10 ? "0" : "") + seconds;
+}
